85b03a01baadcde8226aba64f30153bb67bc71a0
enebulaIT <enebulait@gmail.com>
2023-03-04 12:35:24 +0530

    changes

--- src/components/HomeOne.js
+++ src/components/HomeOne.js
@@ -2,8 +2,6 @@ import React, {Component} from "react";
 import Section from "./layouts/Section";
 import Banner from "./layouts/Banner";
 import MoneyOption from "./layouts/MoneyOption";
-import AboutArea from "./layouts/AboutArea";
-import FunFactArea from "./layouts/FunFactArea";
 
 class HomeOne extends Component {
     render() {
@@ -11,8 +9,6 @@ class HomeOne extends Component {
             <Section design={'home_1'} breadcrumb={false}>
                 <Banner design={'home_1'} />
                 <MoneyOption design={'home_1'} />
-                {/* <FunFactArea /> */}
-                {/* <AboutArea design={'home_1'} /> */}
             </Section>
         );
     }
