1718d69b2a764dfde6c9e53ba5ab87fbc9e2ffc7
enebulaIT <enebulait@gmail.com>
2023-07-27 18:33:52 +0530

    Updates

--- precache-manifest.24eaa60251d1b4e9dc70525f81395419.js
+++ precache-manifest.24eaa60251d1b4e9dc70525f81395419.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b0f049d8376c26df9e515c56dd5afe6",
+    "revision": "db66ab9f6a4366c9c78c013b62cec6de",
     "url": "/index.html"
   },
   {
-    "revision": "276fae0c3da278efec54",
-    "url": "/static/css/main.b22da274.chunk.css"
+    "revision": "ca8700343945ae7df108",
+    "url": "/static/css/main.ca13be1f.chunk.css"
   },
   {
-    "revision": "1e5a724fb62cacd338e0",
-    "url": "/static/js/2.fabf7d4b.chunk.js"
+    "revision": "f24462cb05412721a483",
+    "url": "/static/js/2.8a9d1832.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.fabf7d4b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8a9d1832.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "276fae0c3da278efec54",
-    "url": "/static/js/main.0a2d721c.chunk.js"
+    "revision": "ca8700343945ae7df108",
+    "url": "/static/js/main.671499a5.chunk.js"
   },
   {
     "revision": "50149d8ce0800a19a095",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e655ece92f0f1e568cefe6422b570d82.js"
+  "/precache-manifest.24eaa60251d1b4e9dc70525f81395419.js"
 );
 
 self.addEventListener('message', (event) => {
