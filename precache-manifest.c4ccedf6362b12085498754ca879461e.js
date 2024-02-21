ed46303a6a79af2a6865235850c83d43222e8dd1
Anirudha sah <anirudhasah@Anirudhas-MacBook-Air.local>
2024-02-21 11:38:08 +0530

    Updates

---
+++ precache-manifest.c4ccedf6362b12085498754ca879461e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a35b2e6374afc9231e2f5fbb52de729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7d2cabe8de7ffe1c1d2",
+    "url": "/static/css/main.71bb4c00.chunk.css"
+  },
+  {
+    "revision": "c921c61e9e82f3b99321",
+    "url": "/static/js/2.10218e83.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.10218e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d2cabe8de7ffe1c1d2",
+    "url": "/static/js/main.29791251.chunk.js"
+  },
+  {
+    "revision": "be6e736018471856008b",
+    "url": "/static/js/runtime-main.5bec77c8.js"
+  },
+  {
+    "revision": "6934fc6b58826d7cfe2730923466e2c4",
+    "url": "/static/media/banner3.6934fc6b.jpg"
+  },
+  {
+    "revision": "f9170e5d08708440565036aa4d2d53d4",
+    "url": "/static/media/banner6.f9170e5d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3547c47fdf220c8716562fc381a824c.js"
+  "/precache-manifest.c4ccedf6362b12085498754ca879461e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
