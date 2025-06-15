6c300e465a4c148f58da85eba434ad211fdd0286
Anirudh184 <anirudh.sah96@gmail.com>
2025-06-15 22:26:56 +0530

    director changes

--- src/directorList.js
+++ src/directorList.js
@@ -1,46 +1,42 @@
 export const directorList = [
-    {
-        name: 'Ghanshyambhai G. Dholakiya (C.A.)',
-        designation: 'Chairman'
-    },
-    {
-        name: 'Hareshbhai K. Vora',
-        designation: 'Vice-chairman'
-    },
-    {
-        name: 'Anilbhai R. Desai (Advocate)',
-        designation: 'Director'
-    },
-    {
-        name: 'Dhruvikbhai G. Talavia',
-        designation: 'Director'
-    },
-    {
-        name: 'Shilaben P. Mehta',
-        designation: 'Director'
-    },
-    {
-        name: 'Vijaybhai N. Amlani',
-        designation: 'Director'
-    },
-    {
-        name: 'Nileshbhai K. Parekh (C.A.)',
-        designation: 'Director'
-    },
-    {
-        name: 'Hetalben D. Bavishi',
-        designation: 'Director'
-    },
-    {
-        name: 'Vaibhavbhai C. Mehta',
-        designation: 'Director'
-    },
-    {
-        name: 'Kamleshbhai N. Shah (Advocate)',
-        designation: 'Director'
-    },
-    {
-        name: 'Devjibhai B. Rathod',
-        designation: 'Director'
-    }
+	{
+		name: "Piyushbhai I. Mehta",
+		designation: "Chairman",
+	},
+	{
+		name: "Subhasbhai V. Bavishi",
+		designation: "Vice-chairman",
+	},
+	{
+		name: "Anilbhai R. Desai (Advocate)",
+		designation: "Director",
+	},
+	{
+		name: "Vijaybhai N. Amlani",
+		designation: "Director",
+	},
+	{
+		name: "Kamleshbhai N. Shah (Advocate)",
+		designation: "Director",
+	},
+	{
+		name: "Frenyben N. Parekh (CA)",
+		designation: "Director",
+	},
+	{
+		name: "Kuldeepbhai H. Vora",
+		designation: "Director",
+	},
+	{
+		name: "Chandreshbhai R. Mehta",
+		designation: "Director",
+	},
+	{
+		name: "Devjibhai B. Rathod",
+		designation: "Director",
+	},
+	{
+		name: "Ketanbhai D. Thummar",
+		designation: "Director",
+	},
 ];
